8c08fd00050172429bd4954d5f33248c76180855
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2023-01-04 18:33:37 -0500

    Update rsync-backup-script.sh

--- rsync-backup-script.sh
+++ rsync-backup-script.sh
@@ -1,63 +1,92 @@
-#!/bin/sh
-#Made By: BabyWhale
+import os
+import subprocess
 
-#download sshpass and rsync
-apt install rsync sshpass -y
-
-#user input that reflects if they are updateing an exsisting archive
-echo "Are you updating a current backup archive? (y/n)"
-read -r updatevar
-
-if [ "${updatevar}" = "y" ]; then updatevar="u"
-elif [ "${updatevar}" = "n" ]; then updatevar=""
-fi
-
-
-#user input that reflects if they want ACL to be saved in the archive
-echo "Would you want ACL permissions to be saved? (y/n)"
-read -r aclvar
-
-if [ "${aclvar}" = "y" ]; then aclvar="A"
-elif [ "${aclvar}" = "n" ]; then aclvar=""
-fi
+while True:
+    
+    try:
+        os.system("sudo apt install rsync sshpass")
+        
+    except:
+        print("Failed to Install rsync and sshpass\n")
+        restart_var = input("Press enter to restart or type the word 'exit' to exit the program\n")
+        if restart_var == 'exit': 
+            break
+        else: 
+            continue
 
 
+    ip_var = input("What is the IPv4 address of the remote server?\n")
 
-#public ipv4 of ssh server
-echo "What is the public ipv4 address of the ssh server you are backin up? 22.22.22.22 = an example"
-read -r ipvar 
+    port_var = input("What is the port number that ssh is listening to on the remote server?\n")
 
-#port of ssh server
-echo "What is the port number of the ssh server you are backing up? 22 = an example"
-read -r portvar
+    user_var = input("What is the ssh username? root = an example\n")
 
-#user of ssh server
-echo "What is the username of the ssh server you are backing up? (make sure to use a user who has root access)"
-read -r uservar
+    passwd_var = input("What is the ssh password for this user?\n")
 
-#user password
-echo "What is the password of this user?"
-read -r passwdvar
+    remotedir_var = input("What is the directory of the remote server you are backing up? /home/bob = an example\n")
 
-#DIR copy
-echo "What is the directory of the remote server you would like to backup? /home = an example"
-read -r remotedirvar
+    localdir_var = input("What is the local directory you are saving this to? /root/backups = an example\n")
+    
+    del_var = input("Would you like to delete files that are not on the sending side? (y/n)\n")
 
-#local dir copy
-echo "What is the Local directory location of where you would like to save the backup? /home/bill = an example"
-read -r localdirvar
+    del_var = del_var.lower()
 
-clear
+    if del_var == "y":
+        del_var = " --delete"
 
-echo "this is what the output will be: sshpass -p "$passwdvar" rsync -av$aclvar$updatevar --del -e "ssh -p $portvar" $uservar@$ipvar:$remotedirvar $localdirvar"
-echo "Here is an example for proper syntax: rsync -avAu --del -e "ssh -p 22" root@123.123.321.321:/ /home/backups"
-echo "press enter to execute rsync and a copy of the final script will be called rsync-backup.sh in this current directory"
-read -r emptyvar
+    elif del_var == "yes":
+        del_var = " --delete"
 
+    else:
+        del_var = ""
 
-#Variables from user input is put into proper syntax for rsync and creates file called rsync-backup.sh
-rm rsync-backup.sh
+    print("this is what the output will be: sshpass -p",passwd_var,"rsync -av" + str(del_var),"-e ssh -p",port_var,user_var + "@" + str(ip_var) + ":" + str(remotedir_var),localdir_var)
+    print("The Proper example output is as follows: sshpass -p \"password123\" rsync -av --delete -e \"ssh -p 22\" root@123.123.321.321:/ /home/backups")
+    print("A new file will be created under rsync-backup.sh in this current direcotry for furthor use.")
+    input("Press enter to coninue")
+    
+    try:
+        
+        newfile_var = open("rsync-backup.sh", "w")
+        newfile_var.write("sshpass -p \"" + passwd_var + "\" rsync -av" + del_var + " -e \"ssh -p " + port_var + "\" " + user_var + "@" + ip_var + ":" + remotedir_var + " " + localdir_var)
+        os.system("sudo chmod +x rsync-backup.sh")  
+        
+    except:
+        
+        print("1 or more variables that have been entered may not be correct.\n")
+        print("Failed to save and write to rsync-backup.sh\n")
+        restart_var = input("Press enter to restart or type the word 'exit' to exit the program\n")
+        if restart_var == 'exit': 
+            break
+        else: 
+            continue
 
-echo "sshpass -p \"$passwdvar\" rsync -av$aclvar$updatevar --del -e \"ssh -p $portvar\" $uservar@$ipvar:$remotedirvar $localdirvar" >> rsync-backup.sh
+    askstart_var = input("Would you like to start this script now? (y/n)")
 
-sshpass -p "$passwdvar" rsync -av$aclvar$updatevar --del -e "ssh -p $portvar" $uservar@$ipvar:$remotedirvar $localdirvar
+    askstart_var = askstart_var.lower()
+    
+    if askstart_var == "y":
+        try:
+            print(subprocess.run(["sudo bash rsync-backup.sh"], shell=True))
+            print("Thanks for using this program and the created rsync script is under rsync-backup.sh and can be used at anytime with sudo bash rsync-backup.sh")
+            input("Press enter to exit")
+            break
+        
+        except:
+            print("Failed to run rsync-backup.sh")
+            break
+
+    elif askstart_var == "yes":
+        try:
+            print(subprocess.run(["sudo bash rsync-backup.sh"], shell=True))
+            print("Thanks for using this program and the created rsync script is under rsync-backup.sh and can be used at anytime with sudo bash rsync-backup.sh")
+            input("Press enter to exit")
+            break
+        
+        except:
+            print("Failed to run rsync-backup.sh")
+            break
+    
+    else:
+        print("Thanks for using this program and the created rsync script is under rsync-backup.sh and can be used at anytime with sudo bash rsync-backup.sh")
+        break
